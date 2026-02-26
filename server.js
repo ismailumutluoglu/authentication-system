1f5e0405b6306aa7619ab12bd4fefbdb8af98e72
İsmail Umutluoğlu <ismailumutluoglu10@gmail.com>
2026-02-26 15:42:58 +0300

    add server setup and databse connection

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+// --- MIDDLEWARE'LER ---
+// Gelen JSON body'leri okuyabilmek için
+app.use(express.json());
+// Gelen cookie'leri okuyabilmek için
+app.use(cookieParser());
+
+// --- ROUTES ---
+
+
+// --- SUNUCUYU BAŞLAT ---
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Sunucu ${PORT} portunda çalışıyor`);
+});
